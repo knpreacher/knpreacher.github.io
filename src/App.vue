ae3da8ad41ada6eb6c172620613a1196605e3615
knpreacher <knpreahcer@gmailc.om>
2020-08-14 22:00:55 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-app>
+        <v-main>
+            <Header></Header>
+            <v-container>
+                <v-card>
+                    <v-card-title>kek</v-card-title>
+                    <v-card-text>
+                        <v-icon>home</v-icon>
+                    </v-card-text>
+                </v-card>
+            </v-container>
+            <v-overlay :value="overlay" absolute color="white" opacity="1">
+                <div>
+                    <div class="text-h2 black--text animate__animated animate__backInDown">@knpreacher</div>
+                    <div class="d-flex align-center justify-center">
+                        <div class="text-subtitle-1 text-center animate__animated animate__backInUp black--text mt-3">
+                            Made with love
+                            <v-icon color="red" class="ml-1 animate__animated animate__delay-2s animate__heartBeat">favorite</v-icon>
+                        </div>
+                        <!--<div><v-icon color="red">favorite</v-icon></div>-->
+                    </div>
+                </div>
+            </v-overlay>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+    import Header from "./components/Header";
+
+    export default {
+        name: 'App',
+
+        components: {Header},
+
+        data() {
+            return {
+                overlay: true
+            }
+        },
+
+        mounted() {
+            setTimeout(() => {
+                this.overlay = false
+            }, 3000)
+        }
+    };
+</script>
+
+<!--<style type="scss">-->
+<style type="text/css">
+    /*body {*/
+    /*font-family: "Lucida Console", "Lucida Sans Typewriter", monaco, "Bitstream Vera Sans Mono", sans-serif, default;*/
+    /*}*/
+    /*$body-font-family: */
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="page-header"
+         :style="{'background-image': `url('${backgroundImage}')`}">
+        <div class="fw fh d-flex align-md-end" style="background: rgba(0, 0, 0, 0.6)">
+            <v-container>
+                <v-card flat dark tile
+                        class="d-flex align-center transparent pa-3">
+                    <div >
+                        <v-avatar size="180" class="avatar-wb">
+                            <!--<span>It's me!</span>-->
+                            <v-img src="https://sun9-13.userapi.com/c851432/v851432251/1647ca/aFjBtkzZbQs.jpg"
+                                   alt="It's me!"
+                            ></v-img>
+                        </v-avatar>
+                    </div>
+                    <div class="ml-6 pa-3 text-h2 font-weight-bold">
+                        <!--<span class="font-weight-light">@</span>-->
+                        <!--<span class="font-weight-bold red&#45;&#45;text text&#45;&#45;lighten-3">@</span>-->
+                        knpreacher
+                    </div>
+                </v-card>
+            </v-container>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        data() {
+            return {
+                backgroundImage: require('../assets/knp_logo_g.png'),
+                // avatar: require("https://sun9-13.userapi.com/c851432/v851432251/1647ca/aFjBtkzZbQs.jpg")
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .page-header {
+        background-size: 80px;
+        height: 400px;
+        background-repeat: repeat;
+    }
+
+    .avatar-wb {
+        border: 3px aliceblue solid;
+    }
+</style>
